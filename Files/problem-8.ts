c1f073f02ad8c8333bce6a45d8eeaefca2fa5d95
sifat42000 <rashiquzzamansifat@gmail.com>
2024-11-08 09:28:58 +0600

    my assigment is finished

---
+++ Files/problem-8.ts
@@ -0,0 +1,32 @@
+// Problem-8
+    
+
+// Declear information type
+type infomation = {
+    name : string;
+    age : number;
+    email : string
+}
+
+
+// Declear function 
+function validateKeys<T extends object>(value : T, key : (keyof T)[]){
+     const result = key.filter((k => k in value)).length === key.length;
+     console.log(result)
+     return result
+} 
+
+
+
+// Declear object to information type
+const person : infomation = 
+{ 
+  name: "Alice",
+  age: 25,
+  email: "alice@example.com" 
+}
+
+
+// Call my function
+validateKeys(person,["name", "age"])
+
